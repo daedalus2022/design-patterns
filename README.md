14bddb6203ea2e41afebc22ab8c17d2bd84bff5a
qunwei <qunwei@prevailcloud.com>
2022-12-07 17:52:30 +0800

    add adapter

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
 # design-patterns
-
 refactoringguru.cn 的练习代码
+
+# 运行
+
+- 适配器
+`cargo run --bin adapter`
+
+
+
+
 
 * https://refactoringguru.cn/design-patterns/factory-method/rust/example#example-0
---
+++ adapter/src/main.rs
@@ -0,0 +1,33 @@
+use target::{Target, OrdinaryTarget};
+
+use crate::{adaptee::SpecificTarget, adapter::TargetAdapter};
+
+mod adaptee;
+mod target;
+mod adapter;
+
+/// Calls any object of a `Target` trait.
+///
+/// To understand the Adapter pattern better, imagine that this is
+/// a client code, which can operate over a specific interface only
+/// (`Target` trait only). It means that an incompatible interface cannot be
+/// passed here without an adapter.
+fn call(target: impl Target){
+    println!("'{}'", target.request());
+}
+
+fn main() {
+    let target = OrdinaryTarget;
+
+    print!("A compatible target can be directly called: ");
+    call(target);
+
+    let adaptee = SpecificTarget;
+
+    println!("Adaptee is incompatible with client: '{}'", adaptee.specific_request());
+
+    let adapter = TargetAdapter::new(adaptee);
+    print!("But with adapter client can call its method: ");
+
+    call(adapter);
+}
